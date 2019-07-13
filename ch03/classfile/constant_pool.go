d31c6921b1608517eb32ba11aa0685eb3b2199ac
Tian Kai <gttiankai@163.com>
2019-07-13 23:59:01 +0800

    Update: 完成了一部分 class 文件解析的工作

---
+++ ch03/classfile/constant_pool.go
@@ -0,0 +1,48 @@
+package classfile
+
+import "fmt"
+
+type ConstantPool []ConstantInfo
+
+
+func readConstantInfo(reader *ClassReader) ConstantPool {
+	cpCount := int(reader.readUint16())
+	cp := make([]ConstantInfo, cpCount)
+
+	for i := 1; i < cpCount; i++ {
+		cp[i] = readConstantInfo(reader, cp)
+		switch cp[i].(type) {
+		case *ConstantLongInfo, *ConstantDoubleInfo:
+			i++ // 占两个位置
+		}
+	}
+	return cp
+ }
+
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo  {
+	if cpInfo := self[index]; cpInfo != nil {
+		return cpInfo
+	}
+	panic("Invalid constant pool index")
+}
+
+func (self ConstantPool) getNameAndType(index uint16) (string, string) {
+	ntInfo := self.getConstantInfo(index).(*ConstantNameAndTypeInfo)
+
+	name := self.getutf8(ntInfo.nameIndex)
+	_type := self.getutf8(ntInfo.descriptorIndex)
+	return name, _type
+}
+
+func (self ConstantPool) getClassName(index uint16) string {
+	classInfo := self.getConstantInfo(index).(*ConstantInfo)
+
+	return self.getutf8(classInfo.nameIndex)
+}
+
+func (self ConstantPool) getutf8(index uint16) string {
+	utf8Info := self.getConstantInfo(index).(*ConstantUtf8Info)
+
+	return utf8Info.str
+}
+
---
+++ ch03/classpath/classpath.go
@@ -0,0 +1,100 @@
+package classpath
+
+import "os"
+import "path/filepath"
+
+type ClassPath struct {
+	bootClasspath Entry
+	extClasspath Entry
+	userClasspath Entry
+}
+
+func Parse(jreOption, cpOption string) *ClassPath {
+	cp := &ClassPath{}
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.parseUserClasspath(cpOption)
+	return cp
+}
+
+func (self *ClassPath)parseBootAndExtClasspath(jreOption string)  {
+
+	jreDir := getJreDir(jreOption)
+
+	// jre/lib/*
+
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+
+	// jre/lib/ext/*
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+	self.extClasspath = newWildcardEntry(jreExtPath)
+}
+
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+		return jreOption
+	}
+	if exists("./jar") {
+		return "./jre"
+	}
+	if jh := os.Getenv("JAVA_HOME"); jh != "" {
+		return filepath.Join(jh, "jre")
+	}
+	panic("can not find jre folder!")
+}
+
+func exists(path string) bool  {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func (self *ClassPath) parseUserClasspath(cpOption string)  {
+	if cpOption == "" {
+		cpOption = "."
+	}
+	self.userClasspath = newEntry(cpOption)
+}
+
+// className: fully/qualified/ClassName
+
+func (self *ClassPath) ReadClass(className string) ([]byte, Entry, error)  {
+
+	className = className + ".class"
+
+	if data, entry, err := self.bootClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+
+	if data, entry, err := self.extClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	return self.userClasspath.readClass(className)
+}
+
+func (self *ClassPath) String() string  {
+	return self.userClasspath.String()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
