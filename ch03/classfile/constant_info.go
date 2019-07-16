883b2c43925d7dc7b76622e0f60b14453c8f82de
Tian Kai <gttiankai@163.com>
2019-07-16 09:18:29 +0800

    Update: 完成了mutf8的解析操作

--- ch03/classfile/constant_info.go
+++ ch03/classfile/constant_info.go
@@ -28,3 +28,35 @@ type ConstantInfo interface {
 	readInfo(reader *ClassReader)
 }
 
+func readConstantInfo(reader *ClassReader, cp ConstantPool) ConstantInfo {
+	tag := reader.readUint8()
+	c := new
+}
+
+func newConstantInfo(tag uint8, cp ConstantPool) ConstantInfo {
+
+	switch tag {
+	case CONSTANT_Integer:
+		return &ConstantIntegerInfo{}
+	case CONSTANT_Float:
+		return &ConstantFloatInfo{}
+	case CONSTANT_Long:
+		return &ConstantLongInfo{}
+	case CONSTANT_Double:
+		return &ConstantDoubleInfo{}
+	case CONSTANT_Utf8:
+		return &ConstantUtf8Info{}
+	case CONSTANT_String:
+		return &ConstantStringInfo{}
+	case CONSTANT_Class:
+		return &ConstantClassInfo{}
+	case CONSTANT_Fieldref:
+		return &ConstantFieldrefInfo{}
+	case CONSTANT_Methodref:
+		return &ConstantMethodrefInfo{}
+	case CONSTANT_InterfaceMethodref:
+		return &ConstantInterfaceMethodrefInfo{}
+	}
+
+}
+
