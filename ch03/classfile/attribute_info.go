2e4fb08ad7a97f01efc0531ee2f8ed60305c87ea
Tian Kai <gttiankai@163.com>
2019-07-24 23:53:32 +0800

    写了部分代码, 明天继续

--- ch03/classfile/attribute_info.go
+++ ch03/classfile/attribute_info.go
@@ -12,3 +12,36 @@ type AttributeInfo interface {
 	readInfo(read *ClassReader)
 }
 
+func readAttributes(reader *ClassReader, cp ConstantPool) []AttributeInfo {
+	attributesCount := reader.readUint16()
+	attributes := make([]AttributeInfo, attributesCount)
+
+	for i := range attributes {
+		attributes[i] = readAttribute(reader, cp)
+	}
+	return attributes
+}
+
+func readAttribute(reader *ClassReader, cp ConstantPool) AttributeInfo {
+	attrNameindex := reader.readUint16()
+	attrName := cp.getutf8(attrNameindex)
+	attrLen := reader.readUint32()
+	attrInfo := newAttributeInfo(attrName, attrLen, cp)
+	return attrInfo
+}
+
+func newAttributeInfo(attrName string, attrLen uint32, cp ConstantPool) AttributeInfo {
+	switch attrName {
+	case "Code" : return
+	case "ConstantValue"
+	case "Deprecated"
+	case "Exception"
+	case "LineNumberTable"
+	case "LocalVariableTable"
+	case "SourceFuke"
+	case "Synthetic"
+	default:
+		return &UnparsedAttribute{attrName, attrLen, nil}
+	}
+}
+
